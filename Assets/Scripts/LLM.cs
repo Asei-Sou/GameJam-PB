0d265d102036f947d333231faa28258fff482dae
Toby-Best <38432179+Toby-Best@users.noreply.github.com>
2023-06-30 14:15:07 +0100

    LLM Output now speaks in-game

--- Assets/Scripts/LLM.cs
+++ Assets/Scripts/LLM.cs
@@ -4,14 +4,17 @@ using TMPro;
 
 public class LLM : MonoBehaviour
 {
-    private async void Update()
+    public GameObject inputField;
+    public TextMeshProUGUI output;
+
+    async void Update()
     {
         // If player press enter and the input field is not empty
         if (!Input.GetKeyDown(KeyCode.Return) ||
-            GameObject.Find("InputField (TMP)").GetComponent<TMP_InputField>().text == "") return;
+            inputField.GetComponent<TMP_InputField>().text == "") return;
         
         // take the API key from discord
-        var api = new OpenAIAPI("");
+        var api = new OpenAIAPI("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");
         
         // Not sure which version its using
         var chat = api.Chat.CreateConversation();
@@ -25,11 +28,12 @@ public class LLM : MonoBehaviour
             
         // now let's ask it a question'
         // chat.AppendUserInput("One Two");
-        chat.AppendUserInput(GameObject.Find("InputField (TMP)").GetComponent<TMP_InputField>().text);
+        chat.AppendUserInput(inputField.GetComponent<TMP_InputField>().text);
         
         // get the response
         var response = await chat.GetResponseFromChatbotAsync();
         Debug.Log(response); // "Three"
+        output.text = "\"" + response + "\"";
         
 
     }
